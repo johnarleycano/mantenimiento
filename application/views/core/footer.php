3083cdb6b02055ba7848e993cfd1f30280726116
John Arley Cano Salinas <johnarleycano@hotmail.com>
2018-01-29 14:01:12 -0500

    [0.02.19] Pié de página con versionamiento

--- application/views/core/footer.php
+++ application/views/core/footer.php
@@ -1,3 +1,24 @@
-<center>
-	<a href="#" title="Ir arriba" uk-tooltip uk-totop uk-scroll ></a>
-</center>
+<footer style="
+    background-color: #808080;
+    bottom: 0;
+    color: white;
+    font-size: 0.7em;
+    left: 0;
+	position: fixed;
+    text-align: center;
+    width: 100%;
+">
+	Sistema de administración de permisos de usos de vía | Devimed S.A. | <i>Versión <?php echo version(); ?></i>
+</footer>
+
+<?php
+function version()
+{
+	foreach(array_reverse(glob('.git/refs/tags/*')) as $archivo) {
+    	$contents = file_get_contents($archivo);
+
+	    return basename($archivo);
+	    exit();
+	}
+}
+?>
