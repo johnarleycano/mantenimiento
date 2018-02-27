862ffd7bea8215c1344590ee5620d19e55e45018
John Arley <johnarleycano@hotmail.com>
2018-02-27 15:32:08 -0500

    [0.07.02] style(Panel - Últimas mediciones): mejorada visualización

--- application/views/panel/ultimas_mediciones.php
+++ application/views/panel/ultimas_mediciones.php
@@ -9,20 +9,35 @@ if (count($mediciones) == 0) {
 }
 ?>
 
-<dl class="uk-description-list uk-description-list-divider">
+		<ul  class="uk-list uk-list-striped">
 	<?php foreach ($mediciones as $medicion) { ?>
-		<a onCLick="javascript:ver_detalle(<?php echo $medicion->Fk_Id_Medicion; ?>)" style="text-decoration: none;">
-			<dt><?php echo "$medicion->Sector | $medicion->Via"; ?></dt>
-		    <dd class="uk-text-small">
-				<article class="uk-article">
-				    <p class="uk-article-meta">
-						<time class="timeago" datetime="<?php echo $medicion->Fecha; ?>"><?php echo $medicion->Fecha; ?></time>
-				    </p>
-				</article>
-		    </dd>
-		</a>
+		    <li>
+		    	<span class="uk-text-bold"><?php echo "$medicion->Sector | $medicion->Via"; ?></span>
+		    	<time datetime="<?php echo $medicion->Fecha; ?>" class="uk-text-muted uk-text-small"><?php echo $medicion->Fecha; ?></time><br>
+		    	
+		    	<a onCLick="javascript:ver_detalle(<?php echo $medicion->Fk_Id_Medicion; ?>)" uk-tooltip="pos: bottom-left" title="Ver detalles de la medición" style="text-decoration: none;">
+		    		<i class="fas fa-search"></i>
+		    	</a>
+
+		    	<a onCLick="javascript:generar_pdf(<?php echo $medicion->Fk_Id_Medicion; ?>)" uk-tooltip="pos: bottom-left" title="Imprimir reporte en PDF" style="text-decoration: none;">
+		    		<i class="far fa-file-pdf"></i>
+		    	</a>
+		    </li>
+		
+		<!-- <dt><?php // echo "$medicion->Sector | $medicion->Via"; ?></dt> -->
+		<!-- <a onCLick="javascript:ver_detalle(<?php //echo $medicion->Fk_Id_Medicion; ?>)" style="text-decoration: none;"> -->
+		<!-- <article class="uk-article">
+		    <p class="uk-article-meta">
+		    	<ul class="uk-iconnav">
+				    <time class="timeago" datetime="<?php // echo $medicion->Fecha; ?>"><?php // echo $medicion->Fecha; ?></time>
+				    <li><i class="fas fa-search"></i></li>
+				    <li><i class="fas fa-search"></i></li>
+				</ul>
+		    </p>
+		</article> -->
+		<!-- </a> -->
 	<?php } ?>
-</dl>
+		</ul>
 
 <script type="text/javascript">
 	$("time").timeago();
